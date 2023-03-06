39be97eacd7fc875866bac40884dfc0b2aed8e66
ulasonat <ulasonat11@gmail.com>
2023-03-06 03:53:33 -0500

    Making flake8 changes

--- scripts/discord_bot.py
+++ scripts/discord_bot.py
@@ -4,7 +4,8 @@ import json
 
 
 class DiscordBot(discord.Client):
-    def __init__(self, intents, openai_handler, log_file_path, user_scores_path):
+    def __init__(self, intents, openai_handler, log_file_path,
+                 user_scores_path):
         """
         Initializes a new instance of the DiscordBot class.
         """
@@ -17,15 +18,18 @@ class DiscordBot(discord.Client):
 
     async def on_ready(self):
         """
-        A callback method that is called when the bot connects to the Discord API.
-        Prints a message to the console indicating that the bot has connected to Discord.
+        A callback method that is called when the bot connects
+         to the Discord API.
+        Prints a message to the console indicating that the
+        bot has connected to Discord.
         """
 
         print(f"{self.user} has connected to Discord!")
 
     async def on_message(self, message):
         """
-        A callback method that is called when the bot receives a message from a user.
+        A callback method that is called when the bot
+        receives a message from a user.
         """
 
         if not self.scan_message(message):
@@ -33,7 +37,8 @@ class DiscordBot(discord.Client):
 
         if message.content.startswith("!help"):
             await message.channel.send(
-                "Here, I will provide guidance on how to get the most out this bot."
+                "Here, I will provide guidance on "
+                "how to get the most out this bot."
             )
 
         self.update_log_file(message.author, message.content)
@@ -41,30 +46,34 @@ class DiscordBot(discord.Client):
         scores = self.openai_handler.get_message_score(message.content)
         for label, score in scores.items():
             if not score == -1001:
-                await message.channel.send(label.capitalize() + " score: " + str(score))
+                await message.channel.send(label.capitalize()
+                                           + " score: " + str(score))
             else:
                 scores[label] = 0
                 await message.channel.send(
                     "Something went wrong, your score remained the same."
                 )
 
-        self.update_scores(message.author.id)
+        self.update_scores(message.author.id, scores)
 
     def scan_message(self, message):
         """
-        Returns `True` if the message was sent by a user, and `False` otherwise, and prints the message
+        Returns `True` if the message was sent by a user,
+        and `False` otherwise, and prints the message
         """
         if message.author == self.user:
             return False
 
         return True
 
-    def update_scores(self, user_id):
+    def update_scores(self, user_id, scores):
         """
-        Updates the scores of the given user ID with the scores from the most recent message they sent
+        Updates the scores of the given user ID with the
+        scores from the most recent message they sent
         """
         if user_id not in self.user_scores:
-            self.user_scores[user_id] = self.openai_handler.generate_default_scores()
+            self.user_scores[user_id] = \
+                self.openai_handler.generate_default_scores()
 
         self.save_updated_scores(user_id, scores)
 
