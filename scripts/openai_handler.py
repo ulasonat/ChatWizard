4f7eb37c435ed2c0aacff5014e2b2ba941e40d24
Ulas Onat Alakent <ulas.alakent@columbia.edu>
2023-03-21 18:53:16 -0400

    Revert "PR"

--- scripts/openai_handler.py
+++ scripts/openai_handler.py
@@ -2,14 +2,12 @@ import openai
 
 
 class OpenAIHandler:
-    def __init__(self, api_key, grammar_prompt_path, friendliness_prompt_path, humor_prompt_path):
+    def __init__(self, api_key, grammar_prompt_path):
         """
         Initializes a new instance of the OpenAIHandler class.
         """
         self.api_key = api_key
         self.grammar_prompt_path = grammar_prompt_path
-        self.friendliness_prompt_path = friendliness_prompt_path
-        self.humor_prompt_path = humor_prompt_path
 
         openai.api_key = api_key
 
@@ -19,7 +17,7 @@ class OpenAIHandler:
         """
         # The code below returns a response using OpenAI based on the message.content that will be passed
         response = openai.ChatCompletion.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": content},
             ],
@@ -30,18 +28,14 @@ class OpenAIHandler:
         """
         Generates default scores for each category.
         """
-        return {"grammar": 10,
-                "friendliness": 10,
-                "humor": 10}
+        return {"grammar": 100}
 
     def get_message_score(self, content):
         """
         Processes the text and generates various scores on different categories.
         """
 
-        return {"grammar": self.get_grammar_score(content),
-                "friendliness": self.get_friendliness_score(content),
-                "humor": self.get_humor_score(content)}
+        return {"grammar": self.get_grammar_score(content)}
 
     def get_grammar_score(self, content):
         with open(self.grammar_prompt_path, "r") as file:
@@ -72,81 +66,4 @@ class OpenAIHandler:
         except ValueError:  # meaning API did not produce a pure number
             return -1001
 
-        if grammar_score <= 0:
-            return -1
-        if grammar_score == 0:
-            return 0
-        if grammar_score >= 0:
-            return 1
-
-    def get_friendliness_score(self, content):
-        with open(self.friendliness_prompt_path, "r") as file:
-            prompt = file.read()
-
-        prompt += content
-
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": prompt},
-                ],
-            )
-
-            friendliness_score = response.choices[0]["message"]["content"]
-
-            friendliness_score = int(friendliness_score)
-            friendliness_score = ((friendliness_score / 5) - 1) * 10
-
-            if friendliness_score < -10 or friendliness_score > 10:
-                return -1001
-
-        except openai.error.AuthenticationError:
-            print("No API key provided")
-            return -1001
-
-        except ValueError:  # meaning API did not produce a pure number
-            return -1001
-
-        if friendliness_score <= 0:
-            return -1
-        if friendliness_score == 0:
-            return 0
-        if friendliness_score >= 0:
-            return 1
-
-    def get_humor_score(self, content):
-        with open(self.humor_prompt_path, "r") as file:
-            prompt = file.read()
-
-        prompt += content
-
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "system", "content": prompt},
-                ],
-            )
-
-            humor_score = response.choices[0]["message"]["content"]
-
-            humor_score = int(humor_score)
-            humor_score = ((humor_score / 5) - 1) * 10
-
-            if humor_score < -10 or humor_score > 10:
-                return -1001
-
-        except openai.error.AuthenticationError:
-            print("No API key provided")
-            return -1001
-
-        except ValueError:  # meaning API did not produce a pure number
-            return -1001
-
-        if humor_score <= 0:
-            return -1
-        if humor_score == 0:
-            return 0
-        if humor_score >= 0:
-            return 1
+        return grammar_score
