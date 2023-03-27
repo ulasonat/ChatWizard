f6395676a5b27620444c3e13912e82a5aae7beae
Ulas Onat Alakent <ulas.alakent@columbia.edu>
2023-03-26 20:58:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
 from setuptools import setup
 
-setup(
-    name='chatwizard',
-    version='0.1.0',
-    description="Discord bot to encourage positiveness "
-                "within a server by analyzing and scoring each member's behavior.",
-    author='Ulas Onat Alakent',
-    author_email='ulas.alakent@columbia.edu',
-    maintainer='Ulas Onat Alakent',
-    maintainer_email='ulas.alakent@columbia.edu',
-    url='https://github.com/ulasonat/ChatWizard',
-    install_requires=['openai', 'discord'],
-)
+version = "0.1.1"
+setup()
