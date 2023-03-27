4f1580d8df9ef764899e5b2b603b18b1d0e78102
ulasonat <ulasonat11@gmail.com>
2023-03-26 21:13:02 -0400

    Description change

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 setup(
     name='chatwizard',
     version='0.1.0',
-    description="Discord bot to encourage positiveness "
-                "within a server by analyzing and scoring each member's behavior.",
+    description="Discord bot to encourage positiveness "within a server.",
     author='Ulas Onat Alakent',
     author_email='ulas.alakent@columbia.edu',
     maintainer='Ulas Onat Alakent',
